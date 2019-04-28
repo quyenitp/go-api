195715e86f0760066965bdebb6d10e038e3c2c3e
Quyen Pham <quyenitp@gmail.com>
2019-04-28 20:16:36 +0700

    Add User 3

--- main.go
+++ main.go
@@ -16,12 +16,14 @@ func main() {
 
 	Users = append(Users, models.User{ID: 1, Name: "Jame", AccountIDs: "1,3,5"})
 	Users = append(Users, models.User{ID: 2, Name: "Bond", AccountIDs: "4"})
+	Users = append(Users, models.User{ID: 3, Name: "Jimmy", AccountIDs: "2"})
 
 	var Accounts []models.Account
 	Accounts = append(Accounts, models.Account{ID: 1, UserID: 1, Name: "Tiet Kiem", Balance: 50000})
-	Accounts = append(Accounts, models.Account{ID: 2, UserID: 1, Name: "Thanh Toan", Balance: 10000})
+	Accounts = append(Accounts, models.Account{ID: 2, UserID: 3, Name: "Thanh Toan", Balance: 10000})
 	Accounts = append(Accounts, models.Account{ID: 3, UserID: 1, Name: "Vay Tin Dung", Balance: 150000})
 	Accounts = append(Accounts, models.Account{ID: 4, UserID: 2, Name: "Thanh Toan", Balance: 70000})
+	Accounts = append(Accounts, models.Account{ID: 5, UserID: 1, Name: "Thanh Toan", Balance: 70000})
 
 	controllers.Users = Users
 
